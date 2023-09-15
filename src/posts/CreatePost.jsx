5d84415aefd28f4112b17fc9a50abdf5d0c876c9
Kevin Sundberg <keviin.sundberg@gmail.com>
2023-09-15 16:41:04 +0200

    Create posts,title,tag,content

--- src/posts/CreatePost.jsx
+++ src/posts/CreatePost.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 
 const CreatePost = ({ addPost }) => {
+    const [postTitle, setPostTitle] = useState('');
     const [postContent, setPostContent] = useState('');
+    const [postTags, setPostTags] = useState('');
     const name = "Japp";
     const author = "Haj";
+
+    const handlePostTitleChange = (e) => {
+        setPostTitle(e.target.value);
+    };
+
     const handlePostContentChange = (e) => {
         setPostContent(e.target.value);
+    };
+
+    const handlePostTagsChange = (e) => {
+        setPostTags(e.target.value);
     };
 
     const handleAddPost = async () => {
@@ -15,20 +26,40 @@ const CreatePost = ({ addPost }) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                name, postContent, author
+                title: postTitle,
+                content: postContent,
+                tags: postTags.split(",").map(tag => tag.trim()), // Split tags by comma and trim whitespace
+                name,
+                author
             })
-        })
-        const data = response.json()
-        console.log(data)
+        });
+        const data = await response.json();
+        console.log(data);
 
+        // Clear input fields after submitting
+        setPostTitle('');
+        setPostContent('');
+        setPostTags('');
     };
 
     return (
         <div>
+            <input
+                type="text"
+                placeholder="Title"
+                value={postTitle}
+                onChange={handlePostTitleChange}
+            />
             <textarea
                 value={postContent}
                 onChange={handlePostContentChange}
                 placeholder="Write your post here..."
+            />
+            <input
+                type="text"
+                placeholder="Tags (comma-separated)"
+                value={postTags}
+                onChange={handlePostTagsChange}
             />
             <button onClick={handleAddPost}>Submit</button>
         </div>
