e0b55fbd98bf0751d5b49aa3e0e31eb61f7af069
Vanishah30 <vanisha.shah@gmail.com>
2023-09-15 16:09:32 +0200

    instead of formData using  useState

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,30 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 export default Register;
 
-
-
 function Register() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: ''
-    }); 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-
-    console.log(formData)
-
-    const handleSubmit = async () => {
+  const handleSubmit = async () => {
     try {
       const response = await fetch("http://localhost:8000/users/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ formData }),
+        body: JSON.stringify({ name, email, password }),
       });
       if (response.ok) {
         console.log("Registration successful");
@@ -37,47 +26,45 @@ function Register() {
     }
   };
 
-    return (
-     <div>
-        <h2>Customer Registration</h2>
-        <form onSubmit={handleSubmit}>
-      <div className='form-group'>
-       <label htmlFor="name"> Name:</label>
-            <input
-            
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-            />
-    </div> 
-    
-    <div className='form-group'> 
-        <label htmlFor="email">Email:</label>   
-            <input
-              type="text"  
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}  
-              required
-            />
-            </div>
-    <div className='form-group'>
-        <label htmlFor="password">Password:</label>
-        <input        
-              type="text"  
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            </div>
-            <button type="submit">Register</button>
-        </form>
+  return (
+    <div>
+      <h2>Customer Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">First Name:</label>
+          <input
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
- );
-                
+  );
 }
