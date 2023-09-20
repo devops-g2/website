5c4bf2c69f00b499b8e6962f0d32503dc08af41d
QAsRevenge <hampus.e.molin@gmail.com>
2023-09-20 23:40:02 +0200

    WIP detailed post page

--- pages/DetailedPost.jsx
+++ pages/DetailedPost.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { fetchOnePost } from '../services/fetchPosts'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import '../styles/DetailedPost.css'
 
 export const DetailedPost = () => {
   const { postId } = useParams()
   const [post, setPost] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchDetailedPost = async () => {
@@ -24,8 +28,22 @@ export const DetailedPost = () => {
   }
 
   return (
-    <div>
-      {post.id}, {post.content}, {post.user.name}
+    <div className="detailedPostContext">
+      <FontAwesomeIcon
+        className="back-button"
+        icon={faArrowLeft}
+        onClick={() => navigate(-1)}
+        style={{ cursor: 'pointer' }}
+      />
+      <div className="postContainer">
+        <div className="postAuthor">
+          {post.user.name} <span>{post.created_at}</span>
+        </div>
+        <div className="postTitle">
+          <h2>{post.name}</h2>
+        </div>
+        <div className="postContent">{post.content}</div>
+      </div>
     </div>
   )
 }
