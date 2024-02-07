dcdf1a27d7533cf9d91a77c50540385267a37052
Vanishah30 <vanisha.shah@gmail.com>
2024-02-07 13:32:02 +0100

    Minor changes

--- utils/tests/editpost.test.js
+++ utils/tests/editpost.test.js
@@ -1,5 +1,5 @@
 import { jest, test, expect } from '@jest/globals';
-import { EditAPost } from '../../services/EditPost';
+import { editAPost } from '../../services/EditPost';
 /*import { fetchUser } from './fetchUser'*/
 
 
@@ -18,7 +18,7 @@ test('EditAPost should send a PATCH request and return data', async () => {
   const author = 'abc';
   const postId = 1;
 
-  const result = await EditAPost(postTitle, postContent, author, postId);
+  const result = await editAPost(postTitle, postContent, author, postId);
 
   expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/posts/1', {
     method: 'PATCH',
@@ -40,5 +40,5 @@ test('EditAPost should handle network errors', async () => {
   
   global.fetch = jest.fn().mockRejectedValue(new Error('Network error'));
 
-  await expect(EditAPost('Post', 'Content', 'abc', 1)).rejects.toThrow('Network error');
+  await expect(editAPost('Post', 'Content', 'abc', 1)).rejects.toThrow('Network error');
 });
